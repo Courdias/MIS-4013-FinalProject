49f51ff221be2271d8c2189d4c8c3644e2c9c9de
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-07 12:58:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
   include "header.php";
 ?>
 <div>This is the home page for Final. Navigate to the other pages featuring Javascript libraries in the navbar above!</div>
+<h3>Featured Movie Review title<h3>
+  
+  <h6> Directed by: <a href="director.php" target="_blank">"director name here"</a> / Genre:  <a href="genre.php" target="_blank">"genre name here"</a>
+  <h1> Movie Image here<h1>
+    <div> review text here</div>
+    <div> comments left by other users here<div>
+     
+  
   </body>
 </html>
