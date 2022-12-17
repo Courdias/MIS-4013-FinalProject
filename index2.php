0a445446998a89457717e7a37c565e1e89fa2b25
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 20:58:03 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -2,18 +2,17 @@
   include "header.php";
 ?>
 <div>This is the home page for Final. 
-  Welcome new users! Feel free to explore around and add, edit, or delete any entries from the database.
-  
-  For Dr. Bellah,
-  Here is a checksheet of things that should earn all my points, and where they are:
-  Functionality ("The Back" side)
-  HW 1: Bootstrap & # of Webpages
-  HW 2: Get and Post
-  HW 3: Database Tables, Related Tables, Hyperlink & Post
-  Hw 4: Add, Edit, Delete Functionalities
-  HW 5: Javascript Functions (here, below)
-  Hw 6: Javascript Libraries
-  Appearance(The "front" side)
+  <p>Welcome new users! Feel free to explore around and add, edit, or delete any entries from the database.</p>
+  <p>For Dr. Bellah,</p>
+  <p>Here is a checksheet of things that should earn all my points, and where they are:</p>
+  <p>Functionality ("The Back" side)</p>
+  <p>HW 1: Bootstrap & # of Webpages</p>
+  <p>HW 2: Get and Post</p>
+  <p>HW 3: Database Tables, Related Tables, Hyperlink & Post</p>
+  <p>Hw 4: Add, Edit, Delete Functionalities</p>
+  <p>HW 5: Javascript Functions (here, below)</p>
+  <p>Hw 6: Javascript Libraries</p>
+  <p>Appearance(The "front" side)</p>
      
       <div class="container">
       <p id="date"></p>
