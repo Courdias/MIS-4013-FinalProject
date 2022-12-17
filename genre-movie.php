4d6bf8c63c3b757bd8544cfce964609ae4370896
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 19:58:48 -0600

    Create genre-movie.php

---
+++ genre-movie.php
@@ -0,0 +1,57 @@
+<?php
+  include "header.php";
+?>
+    <h1>Movies with Selected Genre</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Director</th>
+      <th>Genre</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "buidemac_finalproject";
+$password = "finalmovies123";
+$dbname = "buidemac_movies";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$gid = $_POST['id'];
+//echo $iid;
+$sql = "select movie_id, movie_name, d.director_name, g.genre_name from movie m join director d on d.director_id = m.director_id join genre g on g.genre_id = m.genre_id where g.genre_id=" . $gid;
+//echo $sql;
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["movie_id"]?></td>
+    <td><?=$row["movie_name"]?></td>
+    <td><?=$row["director_name"]?></td>
+    <td><?=$row["genre_name"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+ <?php
+  include "gohomebutton.php";
+ ?>
+</html>
