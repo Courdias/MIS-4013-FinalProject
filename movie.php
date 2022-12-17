38c677d584d761528fe6c12c8a197fea6c766d54
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 22:34:57 -0600

    Update movie.php

--- movie.php
+++ movie.php
@@ -27,9 +27,9 @@ if ($conn->connect_error) {
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Edit':
-      $sqlEdit = "update Instructor set instructor_name=? where instructor_id=?";
+      $sqlEdit = "update movie set movie_rating=? where movie_id=?";
       $stmtEdit = $conn->prepare($sqlEdit);
-      $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iid']);
+      $stmtEdit->bind_param("i", $_POST['mRating']);
       $stmtEdit->execute();
       echo '<div class="alert alert-success" role="alert">Instructor edited.</div>';
       break;
@@ -48,24 +48,24 @@ if ($result->num_rows > 0) {
     <td><?=$row["genre_name"]?></td>
     <td><?=$row["movie_rating"]?></td>
     <td>
-              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editStudent<?=$row["student_id"]?>">
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editRating<?=$row["movie_id"]?>">
                 Edit
               </button>
-              <div class="modal fade" id="editStudent<?=$row["student_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editStudent<?=$row["student_id"]?>Label" aria-hidden="true">
+              <div class="modal fade" id="editRating<?=$row["movie_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editRating<?=$row["movie_id"]?>Label" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="editStudent<?=$row["student_id"]?>Label">Edit Student</h1>
+                      <h1 class="modal-title fs-5" id="editRating<?=$row["movie_id"]?>Label">Edit Student</h1>
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                       <form method="post" action="">
                         <div class="mb-3">
-                          <label for="editStudent<?=$row["student_id"]?>Name" class="form-label">Name</label>
-                          <input type="text" class="form-control" id="editStudent<?=$row["student_id"]?>Name" aria-describedby="editStudent<?=$row["student_id"]?>Help" name="sName" value="<?=$row['student_name']?>">
-                          <div id="editStudent<?=$row["student_id"]?>Help" class="form-text">Enter the student's name.</div>
+                          <label for="editRating<?=$row["movie_id"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editRating<?=$row["movie_id"]?>Name" aria-describedby="editRating<?=$row["movie_id"]?>Help" name="mRating" value="<?=$row['movie_rating']?>">
+                          <div id="editRating<?=$row["movie_id"]?>Help" class="form-text">Enter the rating's name.</div>
                         </div>
-                        <input type="hidden" name="iid" value="<?=$row['student_id']?>">
+                        <input type="hidden" name="mRating" value="<?=$row['movie_id']?>">
                         <input type="hidden" name="saveType" value="Edit">
                         <input type="submit" class="btn btn-primary" value="Submit">
                       </form>
