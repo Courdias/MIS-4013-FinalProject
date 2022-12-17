2d6daa112aa4403599d0e0c36b285e0519bb25dc
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-17 00:01:35 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -3,17 +3,7 @@
 ?>
 <div>This is the home page for Final. 
   <p>Welcome new users! Feel free to explore around and add, edit, or delete any entries from the database.</p>
-  <p>For Dr. Bellah,</p>
-  <p>Here is a checksheet of things that should earn all my points, and where they are:</p>
-  <p>Functionality ("The Back" side)</p>
-  <p>HW 1: Bootstrap & # of Webpages</p>
-  <p>HW 2: Get and Post</p>
-  <p>HW 3: Database Tables, Related Tables, Hyperlink & Post</p>
-  <p>Hw 4: Add, Edit, Delete Functionalities</p>
-  <p>HW 5: Javascript Functions (here, below)</p>
-  <p>Hw 6: Javascript Libraries</p>
-  <p>Appearance(The "front" side)</p>
-     
+
       <div class="container">
       <p id="date"></p>
       <button class="btn" onclick='giveDate()'>Show the Time</button>
