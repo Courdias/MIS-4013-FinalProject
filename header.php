778c1a4529a113b681445c6b4ab7b5d6ce929c80
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 23:47:22 -0600

    Update header.php

--- header.php
+++ header.php
@@ -27,7 +27,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="director.php">Reviews by Directors</a>
         </li>
-
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">Back to the Front</a>
+        </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
