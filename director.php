435710558d7adca5fce7fa4841677dcd19ac22bf
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 22:49:16 -0600

    Update director.php

--- director.php
+++ director.php
@@ -7,43 +7,6 @@ $servername = "localhost";
 $username = "buidemac_finalproject";
 $password = "finalmovies123";
 $dbname = "buidemac_movies";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT director_id, director_name from director";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["director_id"]?></td>
-    <td><a href="director-movie.php?id=<?=$row["director_id"]?>"><?=$row["director_name"]?></a></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-</html>
-
-<?php
-  include "header.php";
-?>
-<?php
-$servername = "localhost";
-$username = "buidemac_homework3";
-$password = "homework3user";
-$dbname = "buidemac_homework3";
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
