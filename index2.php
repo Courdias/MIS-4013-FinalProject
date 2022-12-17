53e5f1890d7209c48e6a187da37c41e265054ff8
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-12-16 20:54:16 -0600

    Update index2.php

--- index2.php
+++ index2.php
@@ -11,10 +11,21 @@
   HW 2: Get and Post
   HW 3: Database Tables, Related Tables, Hyperlink & Post
   Hw 4: Add, Edit, Delete Functionalities
-  HW 5: Javascript Functions
+  HW 5: Javascript Functions (here, below)
   Hw 6: Javascript Libraries
   Appearance(The "front" side)
      
-  
+      <div class="container">
+      <p id="date"></p>
+      <button class="btn" onclick='giveDate()'>Show the Time</button>
+    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>  
+<script>
+
+  function giveDate() {
+      const d = new Date();
+      document.getElementById('date').innerHTML = d;
+      }
+        </script>
   </body>
 </html>
